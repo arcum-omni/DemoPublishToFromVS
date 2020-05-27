17c799734d4ab9a21334bb0537f05e8bc9f82894
arcum-omni <travis.l.eiler@gmail.com>
2020-05-27 11:47:56 -0700

    Add Application Insights

--- WebAppDB/Startup.cs
+++ WebAppDB/Startup.cs
@@ -34,6 +34,7 @@ namespace WebAppDB
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+            services.AddApplicationInsightsTelemetry(Configuration["APPINSIGHTS_INSTRUMENTATIONKEY"]);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
